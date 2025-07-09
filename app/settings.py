9f734eb12030a4a62a0553caa8f4a23d5c4f68d6
Sihun Kim <skim585@asu.edu>
2025-07-09 14:53:18 -0700

    feat(app/settings,requirements.txt): connect to postgres, create super user
    connect to postgres, create super user to work around for deployment error.

--- app/settings.py
+++ app/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 from os import environ, path
+import dj_database_url
 
 load_dotenv()
 
@@ -82,10 +83,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default': dj_database_url.config(default=environ.get("DB_URL"))
 }
 
 
