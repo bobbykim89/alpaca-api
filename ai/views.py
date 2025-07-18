6b30fba3667052266cf4c4818a026d2620560655
Sihun Kim <skim585@asu.edu>
2025-07-18 11:10:11 -0700

    feat(ai/views): update the first question
    update the first question to fix degree search result

--- ai/views.py
+++ ai/views.py
@@ -12,18 +12,31 @@ from lib.degree_recommendation import DegreeRecommendation
 
 class CareerQuestionnaireApiView(APIView):
     def get(self, request):
+        # serializer = QuestionSerializer({
+        #     "id": 1,
+        #     "question": "Where are you in your professional development?",
+        #     "options": [
+        #         "high school student",
+        #         "college student",
+        #         "graduate student",
+        #         "early-career professional",
+        #         "late career professional",
+        #         "lifelong learner"
+        #     ],
+        #     "multiple_answers": False
+        # })
         serializer = QuestionSerializer({
-            "id": 1,
-            "question": "Where are you in your professional development?",
-            "options": [
-                "high school student",
-                "college student",
-                "graduate student",
-                "early-career professional",
-                "late career professional",
-                "lifelong learner"
-            ],
-            "multiple_answers": False
+            {
+                "id": 1,
+                "question": "What type of program are you most interested in right now?",
+                "options": [
+                    "Bachelor's degree",
+                    "Graduate degree (e.g., Master's or PhD)",
+                    "Certificate or short-term credential",
+                    "I'm just exploring for now"
+                ],
+                "multiple_answers": False
+            }
         })
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
